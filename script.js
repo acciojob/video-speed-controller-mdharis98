93641ef034515cec9820a99274ad3b67caa859fd
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-10-02 16:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const player = document.querySelector(".player");
+const player = document.querySelector(".player__video");
       const video = player.querySelector(".viewer");
       const progress = player.querySelector(".progress");
-      const progressBar = player.querySelector(".progress__filled");
+      const progressBar = player.querySelector(".rewind");
       const toggle = player.querySelector(".toggle");
       const ranges = player.querySelectorAll(".player__slider");
       const skipButtons = player.querySelectorAll("[data-skip]");
